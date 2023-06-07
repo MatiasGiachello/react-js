0bea402a8ea601903381380736baa3db70b8bbf0
Matias Giachello <matiasgiachello04@gmail.com>
2023-06-07 18:04:28 -0300

    CartWidget

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,12 +1,11 @@
-
-import './cartwidget.css'
+import './CartWidget.css'
 
 const CartWidget = () => {
     const imgcarrito = "https://w7.pngwing.com/pngs/225/984/png-transparent-computer-icons-shopping-cart-encapsulated-postscript-shopping-cart-angle-black-shopping-thumbnail.png"
 
   return (
     <div>
-        <img className='imgCarrito' src="{imgCarrito}" alt="carritos de compras " />
+        <img className='imgCarrito' src={imgcarrito} alt="carritos de compras " />
         <strong>12</strong>
     </div>
   )
